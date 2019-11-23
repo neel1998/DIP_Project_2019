97bd671b369517846f516f2783d0736ecd89d404
neel1998 <neellovepainting@gmail.com>
2019-11-24 01:08:15 +0530

    minor change

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,6 +7,8 @@ def hello():
 
 @app.route("/upload1",methods=['POST'])
 def upload1():
+	filelist = request.files.getlist('file')
+	print(filelist[0].path)
 	return "got file"
 if __name__ == "__main__":
 	app.run(debug=True)

28495573bf5b52cdcf2e2767f993fc7ce6562857
neel1998 <neellovepainting@gmail.com>
2019-11-24 09:25:27 +0530

    file saving added

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import os
+
+image_path = './upload_images/'
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+    return render_template('index.html',files=[])
 
 @app.route("/upload1",methods=['POST'])
 def upload1():
 	filelist = request.files.getlist('file')
-	print(filelist[0].path)
-	return "got file"
+	file = filelist[0]
+	fn = file.filename
+	print(fn)
+	file.save(image_path+fn)
+	return render_template('index.html',files=[fn])
 if __name__ == "__main__":
 	app.run(debug=True)
